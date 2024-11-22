63670dee2d58104d94a40b352d02ae3927087eca
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-11-22 15:52:16 -0300

    octavo commit

---
+++ assets/js/Credenciales.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBo7Vw8KATBsq7MkoRjpNTkOcN77TJ5i_Q",
+    authDomain: "prueba3progafrontend.firebaseapp.com",
+    projectId: "prueba3progafrontend",
+    storageBucket: "prueba3progafrontend.firebasestorage.app",
+    messagingSenderId: "367544713158",
+    appId: "1:367544713158:web:e854c59d7dc8aefb713cc7"
+  };
